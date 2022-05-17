c7621e9a6128d9b2edc125685a96c66d2c386fac
kesshei <dagang520@qq.com>
2022-05-17 16:18:30 +0800

    提交demo
    第一次提交,做了一个demo信息。

---
+++ WebView2Demo/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace WebView2Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(800, 450);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WebView2Demo/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebView2Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Resize += new EventHandler(Form_Resize);
+            webView21.CoreWebView2InitializationCompleted += WebView21_CoreWebView2InitializationCompleted;
+            Initialize();
+        }
+        /// <summary>
+        /// 实现自适应页面缩放
+        /// </summary>
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView21.Size = ClientSize - new Size(webView21.Location);
+        }
+        /// <summary>
+        /// webview 加载完毕
+        /// </summary>
+        private void WebView21_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            webView21.CoreWebView2.Navigate("https://www.baidu.com/");
+        }
+        /// <summary>
+        /// WebView2初始化
+        /// </summary>
+        async void Initialize()
+        {
+            var result = await CoreWebView2Environment.CreateAsync(null, Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cache"), null);
+            await webView21.EnsureCoreWebView2Async(result);
+        }
+
+        /// <summary>
+        /// 点击按钮
+        /// </summary>
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            //开启开发者工具 (可以通过右键，检查页面实现打开开发者工具)
+            // webView21.CoreWebView2.OpenDevToolsWindow();
+
+            //填充搜索内容
+            await webView21.CoreWebView2.ExecuteScriptAsync("document.querySelector('#kw').value='1234'");
+            //启动搜索
+            await webView21.CoreWebView2.ExecuteScriptAsync("document.querySelector('#su').click();");
+        }
+    }
+}
